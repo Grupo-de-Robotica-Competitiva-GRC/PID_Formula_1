d7eb39f140207da0c659e874b74d1a2f2b8dd3e9
Gabe0007 <grodrigoba@gmail.com>
2024-11-21 16:56:35 -0300

    90º força bruta, inconsistências em curvas

--- PID_arduino_nano/PID_NANO_FDC.ino
+++ PID_arduino_nano/PID_NANO_FDC.ino
@@ -1,10 +1,6 @@
 // Definição dos pinos para controle dos motores com a ponte H L298
 #define M1_PWM 9   // Pino PWM para o 1º Motor
 #define M2_PWM 6   // Pino PWM para o 2º Motor
-#define dir1A 10    // Direção do 1º Motor A
-#define dir1B 11    // Direção do 1º Motor B
-//#define dir2A 7     // Direção do 2º Motor A
-#define dir2B 8     // Direção do 2º Motor B
 
 // Definição dos pinos dos sensores de linha
 #define pin_S1 7   // Sensor 1
@@ -16,7 +12,7 @@
 
 //----- CONSTANTES -----
 float Ki = 0; // para não acumular tanto erro
-int Kp = 35;   // valor padrão para nós
+int Kp = 50;   // valor padrão para nós
 int Kd = 98;   // ir ajustando até ficar bom
 
 int P = 0, I = 0, D = 0, PID = 0; // variáveis PID
@@ -26,7 +22,8 @@ int erro = 0;  // cálculo do erro
 // Velocidades padrão/max
 int aVelo = 250;
 int bVelo = 250;
-int velocidadeBase = 250;
+int velocidadeBase = 150;
+int sensores_ativos = 0;
 
 int sensor_valores[6] = {0,0,0,0,0,0};
 
@@ -34,15 +31,6 @@ void setup() {
   // Configuração dos pinos dos motores
   pinMode(M1_PWM, OUTPUT);  // PWM do Motor 1
   pinMode(M2_PWM, OUTPUT);  // PWM do Motor 2
-  pinMode(dir1A, OUTPUT);   // Direção do Motor 1 A
-  pinMode(dir1B, OUTPUT);   // Direção do Motor 1 B
-  //pinMode(dir2A, OUTPUT);   // Direção do Motor 2 A
-  pinMode(dir2B, OUTPUT);   // Direção do Motor 2 B
-
-  //digitalWrite(dir1A, HIGH);
-  //digitalWrite(dir1B, LOW);
-  //digitalWrite(dir2A, HIGH);
-  //digitalWrite(dir2B, LOW);
 
   // Configura os pinos dos sensores como entrada
   pinMode(pin_S1, INPUT);
@@ -71,7 +59,7 @@ void calcular_erro(){
   erro = 0;
   int pesos[6] = {-10, -4, -1, 1, 4, 10};
   int erro_total = 0;
-  int sensores_ativos = 0;
+  sensores_ativos = 0;
 
   for(int i = 0; i < 6; i++){
     if(sensor_valores[i] == 1){
@@ -81,7 +69,8 @@ void calcular_erro(){
   }
 
   if(sensores_ativos > 0)
-    erro = erro_total / sensores_ativos;
+    //erro = erro_total / sensores_ativos;
+    erro = erro_total;
   else
     erro = erroAnterior;
 }
@@ -110,12 +99,6 @@ void controlaMotor() {
   velEsq = constrain(velEsq, 0, 255);
   velDir = constrain(velDir, 0, 255);
 
-  // Definir a direção dos motores
-  digitalWrite(dir1A, HIGH);
-  digitalWrite(dir1B, LOW);
-  //digitalWrite(dir2A, HIGH);
-  digitalWrite(dir2B, LOW);
-
   // Ajustar a velocidade dos motores
   analogWrite(M1_PWM, velEsq);
   analogWrite(M2_PWM, velDir);
@@ -138,18 +121,13 @@ void loop() {
   
   // Calcula o erro com base na posição
   calcular_erro();
-  //if(pare == false){
-    // Calcula o PID para controlar os motores
-    calculaPID();
 
-    // Controla os motores com base no PID calculado
-    controlaMotor();
-  /*}else{
-    velEsq = 40;
-    velDir = 45;
-    analogWrite(M1_PWM, velEsq);  // Ajusta a velocidade do motor 1
-    analogWrite(M2_PWM, velDir);  // Ajusta a velocidade do motor 2
-  }*/
+  // Calcula o PID para controlar os motores
+  calculaPID();
+
+  // Controla os motores com base no PID calculado
+  controlaMotor();
+
   
   // Imprime os valores para monitoramento
   Serial.print(erro);
@@ -167,5 +145,10 @@ void loop() {
   Serial.print("\tKi: ");
   Serial.println(Ki);
 
-  delay(20);
+
+  if((erro < -14 || erro >= 14) && sensores_ativos>=4){
+    delay(300);
+  }else{
+    delay(20);
+  }
 }
