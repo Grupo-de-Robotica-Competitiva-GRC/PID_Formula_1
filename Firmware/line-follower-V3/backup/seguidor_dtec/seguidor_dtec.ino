7d2a5b015aaf76f64a09619b8d1c801d55d47f40
Vitor-MB <americovitor1234@gmail.com>
2025-10-31 02:19:07 -0300

    organizando o codigo backup

--- Firmware/line-follower-V3/backup/seguidor_dtec/seguidor_dtec.ino
+++ Firmware/line-follower-V3/backup/seguidor_dtec/seguidor_dtec.ino
@@ -1,12 +1,14 @@
+//BIBLIOTECAS
 #include <Arduino.h>
 #include <QTRSensors.h>
 #include "BluetoothSerial.h"
-// #include <esp32-hal-lesdc.h>
 #include "driver/ledc.h"
 
+//BLUETOOTH
 BluetoothSerial SerialBT;
 String inputString = "";
 
+//PINOS
 #define EMITTER_PIN    15
 #define LED_CALIBRANDO 2
 #define LED_SEGUIDOR   4
@@ -14,8 +16,8 @@ String inputString = "";
 #define BOTAO_SEGUIR   13
 #define MOTOR_A1       22
 #define MOTOR_A2       21
-#define MOTOR_B1       19
-#define MOTOR_B2       18
+#define MOTOR_B1       18
+#define MOTOR_B2       19
 
 
 QTRSensors qtr;
@@ -25,15 +27,17 @@ bool modoSeguidor = false;
 bool calibrando = false;
 uint8_t  sensorsPin[8] = {34, 35, 32, 33, 27, 26, 25, 14};
 uint16_t sensorValues[8];
-float Ki = 0; // para não acumular tanto erro
-int Kp = 21;  //ajustar paramento
-int Kd = 35;  //ajusta tamebem algem ve tutorial como ajustar.
+float Ki = 0; 
+int Kp = 21;  
+int Kd = 35;  
 
 int P = 0, I = 0, D = 0, PID = 0; // variáveis PID
 int16_t velEsq = 0, velDir = 0, erroAnterior = 0; // controle dos motores
 int16_t erro = 0;  // cálculo do erro
 int bVelo = 700, aVelo =700;
 
+
+//================FUNÇÕES PARA MOVIMENTAR OS MOTORES================
 void move_motorA(int16_t vel)
 {
   vel *= 0.65;
@@ -50,11 +54,8 @@ void move_motorA(int16_t vel)
   }
 
 }
-
 void move_motorB(int16_t vel)//vel e duty
 {
-  vel *= -1;//o motor B  o da esquerda olhando de fren tem menos torque por isso o fator. 
-  //TODO tem que fazer um umento de potencia gradual nesse motor  para ele nao sair no supetao para esquerda o torque dele  e alto.
   if (vel >= 0)
   {
     ledcWrite(MOTOR_B1, vel);
@@ -75,12 +76,14 @@ void pararMotores()
   move_motorB(0);
 }
 
+//================CALCULO DO PID================
 void calculaPID() {
   //if (erro == 0) { I = 0; }
-  P = erro;
   I = I + erro; // Acúmulo de erro (somatório)
   if (I > 255) { I = 255; }
   else if (I < -255) { I = -255; }
+  
+  P = erro;
   D = erro - erroAnterior; // PID discreto
   PID = (Kp * P) + (Ki * I) + (Kd * D);
   erroAnterior = erro;
@@ -89,11 +92,9 @@ void calculaPID() {
 
 void calcula_erro()
 {
-
-  /* americo ou quem for  olahr esse codigo esse array de erro pode/temque ser ajsutado com base em dados empiricos ma sde inicio acho é suficite
-   pS: nao descomenta esse demtro*/
   erro = 0;
   // float erro_pesos[8] = {-(1 / 50), -(1 / 70), (-1 / 100), -(1 / 120),  (1 / 120), (1 / 100), (1 / 70), (1 / 50) };
+  
   float erro_pesso[8] = {-100, -16, -7, 0, 0, 7, 16, 100};
   qtr.read(sensorValues);
   for (uint8_t i = 0; i < 8; i++)
@@ -104,10 +105,6 @@ void calcula_erro()
     }
   }
   Serial.println(erro);
-  // for(uint8_t i = 0; i < 8; i++)  
-  // {
-  //   erro += erro_pesso[i] * sensorValues[i];
-  // }
 }
 
 
@@ -120,12 +117,9 @@ void controlaMotor() {
     velDir = aVelo + PID;
   }
 
-  // if(velEsq < 0) velEsq = 0;
-  // if(velDir < 0) velDir = 0;
-  //en teiirua e spararMotores();o colocar os valores de vel em seus respectivos motores
-
 }
 
+//================CALIBRAÇÃO================
 void moveCalibrar(void *pvParams) {
   int16_t vel = 512;
   digitalWrite(LED_CALIBRANDO, HIGH);
@@ -162,6 +156,8 @@ void calibrar(void *pvParams) {
   }
 }
 
+
+//================COMUNICAÇÃO BLUETOOTH================
 void lendoMensagem(String cmd) {
   cmd.trim();
 
@@ -195,6 +191,8 @@ void atualiza_variaveis(){
   }
 }
 
+
+//================SEGUIDOR================
 void seguidor() {
   delay(400);
   digitalWrite(LED_SEGUIDOR, HIGH);
@@ -224,7 +222,7 @@ void seguidor() {
 
 void setup() {  
     Serial.begin(115200);    // GPIO18 no canal 0
-    // ledcWrite(18, 128);            // 50% duty cycle4
+
     qtr.setTypeAnalog();
     qtr.setSensorPins(sensorsPin, 8);
     qtr.setEmitterPin(EMITTER_PIN);
@@ -232,16 +230,11 @@ void setup() {
     pinMode(LED_SEGUIDOR, OUTPUT);
     pinMode(BOTAO_CALIBRAR, INPUT);
     pinMode(BOTAO_SEGUIR, INPUT);
-    // pinMode(EMITTER_PIN, OUTPUT); 
-    Serial.println("presine o botao para calibra");
+   
     ledcAttachChannel(MOTOR_A1, 2000, 10, 0);
     ledcAttachChannel(MOTOR_A2, 2000, 10, 1);
     ledcAttachChannel(MOTOR_B1, 2000, 10, 2);
     ledcAttachChannel(MOTOR_B2, 2000, 10, 3);
-    // ledcAttachChannel(2, 250, 8, 4);
-    // digitalWrite(EMITTER_PIN, HIGH);
-    // ledcAttachChannel(15, 250 , 8, 5);
-    // ledcWrite(15, 120);
 
     SerialBT.begin("Barão_Vermelho");
     Serial.println("Bluetooth iniciado 'Barão_Vermelho'");
